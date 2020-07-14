3d1650e78315e0c6b6fd3ea53a277fe31e473921
Milo123459 <milo5huntington@gmail.com>
2020-07-14 16:31:08 +0100

    New commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fetch = require("node-fetch");
 /**
- * 
+ *
  * @param {String} sub The subreddit
  * @param {Boolean} [verb] Verbose logging
  * @example
  * await api("meme", true); //Verbose logging
- * @Milo123459 
+ * @Milo123459
  */
 module.exports = async function (sub, verb) {
   if (!sub) throw new TypeError("Error, you did not specify a subreddit!");
@@ -13,7 +13,8 @@ module.exports = async function (sub, verb) {
   let a = await fetch(
     `https://image-api-2.glitch.me/reddit/${sub}`
   ).then((res) => res.json());
-  if(verb) console.log(`[IMAGEAPI.JS]: TOOK ${Date.now()-start}MS TO RECIEVE YOUR IMAGE.`)
+  let x = Date.now() - start;
+  if (verb) console.log(`[IMAGEAPI.JS]: TOOK ${x}MS TO RECIEVE YOUR IMAGE. `);
   if (a.error)
     throw new TypeError("Error, probably due to an invalid subreddit!");
   return a.img;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,3 @@
-const imageapi = require("../index");
-
-async function test() {
-  console.log(await imageapi(`meme`, true));
-}
-test();
+/*
+imageapi.js test file
+*/
